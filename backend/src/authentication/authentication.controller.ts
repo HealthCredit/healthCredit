b1834678a48ff3912ea425247eb003e84f739957
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-03-16 15:59:28 +0100

    user auth
    - packing user signed message with solidityKeccak256

--- backend/src/authentication/authentication.controller.ts
+++ backend/src/authentication/authentication.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Nonce } from './types/index'
+import { WalletDto, SignatureDto } from './dto/index'
+import { AuthenticationService } from './authentication.service';
+import { Controller, Get, Body, Post } from '@nestjs/common';
 
-@Controller('authentication')
-export class AuthenticationController {}
+@Controller('api')
+export class AuthenticationController {
+  constructor(
+    private readonly authService: AuthenticationService,
+  ) {}
+
+  @Get('nonce')
+  generateNonce(@Body() walletData: WalletDto): Promise<Nonce> {
+    return this.authService.generateNonce(walletData)
+  }
+
+  @Post('signature')
+  verifySignature(@Body() signatureDto: SignatureDto): Promise<boolean> {
+      return this.authService.verifySignature(signatureDto)
+  }
+}
---
+++ backend/src/authentication/constants/constant.ts
@@ -0,0 +1,6 @@
+export const SIGNING_MESSAGE = `Health Credits
+
+Sign Message
+
+Nonce: {NONCE_VALUE}
+`
