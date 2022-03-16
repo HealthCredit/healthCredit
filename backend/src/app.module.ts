ffa0e7ae000ca1ddca64e1f7cffca21967c41a8b
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-03-16 16:53:14 +0100

    schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthenticationModule],
-  controllers: [AppController],
+  imports: [AuthenticationModule, PrismaModule],
+controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
