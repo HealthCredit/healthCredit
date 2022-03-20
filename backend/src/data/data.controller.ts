5d1469367bf04913d29628ae1152b0aecade2afa
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-03-20 05:04:20 +0100

    upload to web3.storage
    Setting up upload functionality

--- backend/src/data/data.controller.ts
+++ backend/src/data/data.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { DataService } from './data.service';
 
 @Controller('data')
 export class DataController {
   constructor(private readonly dataService: DataService) {}
 
-  @Get('token')
-  getAccessToken() {
-    return this.dataService.getAccessToken();
+  @Post('upload')
+  storeFiles() {
+    return this.dataService.storeFiles();
+  }
+
+  @Get('check')
+  async checkStatus() {
+    return this.dataService.checkStatus();
+  }
+
+  @Get('retrieve')
+  async retrieve() {
+    return this.dataService.retrieve();
   }
 }
--- backend/src/data/data.service.ts
+++ backend/src/data/data.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { Web3Storage } from 'web3.storage';
+import { Blob } from 'buffer';
+import { Web3Storage, getFilesFromPath } from 'web3.storage';
 import { ConfigService } from '@nestjs/config';
+import { appendFile, readFileSync, createReadStream } from 'fs';
+import { CarReader } from '@ipld/car';
 
+import { encode } from 'multiformats/block';
+import * as cbor from '@ipld/dag-cbor';
+import { sha256 } from 'multiformats/hashes/sha2';
 @Injectable()
 export class DataService {
   constructor(private config: ConfigService) {}
@@ -13,5 +19,69 @@ export class DataService {
   async makeStorageClient() {
     const token = await this.getAccessToken();
     return new Web3Storage({ token });
+  }
+
+  async makeFileObjects() {
+    const obj = { hello: 'world' };
+    const blob = new Blob([JSON.stringify(obj)], { type: 'application/json' });
+    const file = await blob.text();
+
+    const files = [
+      await appendFile(
+        'plain-utf8.txt',
+        'content-of-file-1',
+        'utf-8',
+        (err) => {
+          if (err) throw err;
+          console.log('data uploaded');
+        },
+      ),
+      // new File([file], 'hello.json'),
+      await appendFile('hello.json', file, (err) => {
+        if (err) throw err;
+        console.log('data uploaded');
+      }),
+    ];
+    return files;
+  }
+
+  async encodeCborBlock(value) {
+    return encode({ value, codec: cbor, hasher: sha256 });
+  }
+
+  async makeCar(rootCID, ipldBlocks) {
+    return new CarReader(rootCID, ipldBlocks);
+  }
+
+  async storeFiles() {
+    const files: any = await getFilesFromPath('file');
+    for (const f of files) {
+      console.log(f);
+      // { name: '/path/to/me', stream: [Function: stream] }
+    }
+
+    const web3Storage = await this.makeStorageClient();
+    const cid = await web3Storage.put(files);
+    console.log(`stored ${files.length} files. cid: ${cid}`);
+  }
+
+  async checkStatus() {
+    const client = await this.makeStorageClient();
+    const status = await client.status(
+      'bafybeieg7qchsucyqdei7flw7bzgfoqyostkfvjfoo4u6fc4ipmiqpiuim',
+    );
+
+    console.log(status);
+  }
+
+  async retrieve() {
+    const cid = 'bafybeieg7qchsucyqdei7flw7bzgfoqyostkfvjfoo4u6fc4ipmiqpiuim'
+    const client = await this.makeStorageClient();
+    const res = await client.get(cid);
+    console.log(`Got a response! [${res.status}] ${res.statusText}`);
+    if (!res.ok) {
+      throw new Error(`failed to get ${cid}`);
+    }
+    console.log(res);
   }
 }
