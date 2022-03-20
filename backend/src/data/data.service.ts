202df0f008767853f1b635816c3e1a9dc5b0c92b
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-03-20 17:23:48 +0100

    metadata
    - inject image uri to metadata.json and upload to filecoin via web3.storage

--- backend/src/data/data.service.ts
+++ backend/src/data/data.service.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaService } from './../prisma/prisma.service';
 import { AuthDto } from '../authentication/dto';
 import { CidDto } from './dto/cid.dto';
+import { writeFileSync, readdir, readFileSync } from 'fs';
 @Injectable()
 export class DataService {
   constructor(private config: ConfigService, private prisma: PrismaService) {}
@@ -22,6 +23,53 @@ export class DataService {
   }
 
   /**
+   * @param
+   * @returns
+   * */
+  async modifyMetadata(wallet: string) {
+    // check that user exists
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        walletAddress: wallet,
+      },
+    });
+
+    // fetch files uploaded to a folder
+    const files: any = await getFilesFromPath(`folder/${wallet}/LYS.png`);
+    const cid = await this.uploadToFilecoin(files, existingUser);
+
+    const image = `https://${cid}.ipfs.dweb.link/LYS.png`;
+
+    readdir(`folder/${wallet}`, (err, files) => {
+      if (err) console.log(err);
+      else {
+        files.forEach((file) => {
+          if (file === 'metadata.json') {
+            const content = readFileSync(`folder/${wallet}/${file}`, {
+              encoding: 'utf8',
+              flag: 'r',
+            });
+
+            const dataL = JSON.parse(content);
+            const data = { image };
+
+            const contentNew = Object.assign(dataL, data);
+            // console.log(contentNew);
+
+            writeFileSync(
+              `folder/${wallet}/${file}`,
+              JSON.stringify(contentNew),
+              'utf-8',
+            );
+          }
+        });
+      }
+    });
+
+    return image;
+  }
+
+  /**
    *  @param dto: user wallet address
    *  @returns projectUri
    * */
@@ -33,29 +81,21 @@ export class DataService {
       },
     });
 
-    // if user exists...
-    if (existingUser) {
+    // if user cid does not exist...
+    if (!existingUser.cid) {
+      // create imageUri for metadata and add it to metadata.json
+      await this.modifyMetadata(dto.walletAddress);
+
       // fetch files uploaded to a folder
       const files: any = await getFilesFromPath(`folder/${dto.walletAddress}`);
-      const web3Storage = await this.makeStorageClient();
-      console.log(`🤖 Storing simple CBOR object...`);
-
-      // save files to filecoin via web3.storage with
-      // same structure -> cid/walletAddress/file/[...project files...]
-      const cid = await web3Storage.put(files);
-
-      // store the cid to the repective address on postgres database
-      await this.saveCid(existingUser.id, cid);
-      console.log(`🎉 Done storing simple CBOR object. CID: ${cid}`);
-      console.log(`💡 If you have ipfs installed, try: ipfs dag get ${cid}\n`);
-      console.log(`stored ${files.length} files. cid: ${cid}`);
+      const cid = await this.uploadToFilecoin(files, existingUser);
 
       // variable for upload link
       const projectUri = `https://${cid}.ipfs.dweb.link/${dto.walletAddress}`;
 
       return { projectUri };
     } else {
-      throw new Error('Unauthorized access!');
+      throw new Error('Files already uploaded!');
     }
   }
 
@@ -85,13 +125,8 @@ export class DataService {
     }
 
     // variables to hold metadat uri
-    const metadata_uri = `https://${cid}.ipfs.dweb.link/${walletAddress}/file/metadata.json`;
+    const metadata_uri = `https://${cid}.ipfs.dweb.link/${walletAddress}/metadata.json`;
     const uri = { metadata_uri };
-
-    const files = await res.files();
-    for (const file of files) {
-      console.log(`${file.cid} -- ${file.name} -- ${file.size}`);
-    }
 
     return uri;
   }
@@ -104,6 +139,14 @@ export class DataService {
    */
   async getAccessToken(): Promise<string> {
     return this.config.get<string>('web3Storage');
+  }
+
+  /**
+   *  @returns new Web3Storage instance
+   */
+  async makeStorageClient() {
+    const token = await this.getAccessToken();
+    return new Web3Storage({ token });
   }
 
   /**
@@ -121,10 +164,22 @@ export class DataService {
   }
 
   /**
-   *  @returns new Web3Storage instance
-   */
-  async makeStorageClient() {
-    const token = await this.getAccessToken();
-    return new Web3Storage({ token });
+   * @param file
+   * */
+  async uploadToFilecoin(files: any, existingUser: any) {
+    const web3Storage = await this.makeStorageClient();
+    console.log(`🤖 Storing simple CBOR object...`);
+
+    // save files to filecoin via web3.storage with
+    // same structure -> cid/walletAddress/file/[...project files...]
+    const cid = await web3Storage.put(files);
+
+    // store the cid to the repective address on postgres database
+    await this.saveCid(existingUser.id, cid);
+    console.log(`🎉 Done storing simple CBOR object. CID: ${cid}`);
+    console.log(`💡 If you have ipfs installed, try: ipfs dag get ${cid}\n`);
+    console.log(`stored ${files.length} files. cid: ${cid}`);
+
+    return cid;
   }
 }
