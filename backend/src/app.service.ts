23f1624cdb947c56fb4cb06e8e99f48a8ce39c47
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-05-22 20:45:55 +0100

    Update app.service.ts

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! GTF Flat-Earthers!!!!';
+    return 'Hello World!';
   }
 }
